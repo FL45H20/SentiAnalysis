c1245c9e792a4e20e022cbfbd1687f2b980845e3
FL45H20 <59165231+FL45H20@users.noreply.github.com>
2020-02-23 13:16:44 +0530

    Update sentianalysis.py
    used cmd line args instead of simple i/p

--- sentianalysis.py
+++ sentianalysis.py
@@ -6,14 +6,19 @@ from builtins import *
 #import textblob for sentimnet analysis
 from textblob import TextBlob
 
+#for passing text as cmdline arg
+import sys
+
 #get user i/p for para to analyze
-para = input("Please provide a text:")
-print (para)
+#get sentiment for each cmd line arg
+for i in range(len(sys.argv) -1):
+  para = sys.argv[i+1]
+  print (para)
 
-#create a TextBlob object
-para =  TextBlob(para)
+  #create a TextBlob object
+  para =  TextBlob(para)
 
-#get sentinent
-#Polarity is float which lies in the range of [-1,1] where 1 means positive
-#Subjectivity is float liew beween [0,1] where 1 means personal opinion, emotion or judgment whereas 0 means objective which is factual information
-print ("Polarity: " + str(para.sentiment.polarity) + " Subjectivity: " + str(para.sentiment.subjectivity))
+  #get sentinent
+  #Polarity is float which lies in the range of [-1,1] where 1 means positive
+  #Subjectivity is float liew beween [0,1] where 1 means personal opinion, emotion or judgment whereas 0 means objective which is factual infor>
+  print ("Polarity: " + str(para.sentiment.polarity) + " Subjectivity: " + str(para.sentiment.subjectivity))
